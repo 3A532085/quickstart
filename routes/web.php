58636e11a3aa82cd6be309de999f260c36b169c8
江珮妤 <Luxgen077@gmail.com>
2018-10-16 17:02:45 +0800

    建置路由(Route)

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
+//顯示我們所有任務的清單
 Route::get('/', function () {
-    return view('welcome');
+    //
 });
+
+// 增加新的任務
+Route::post('/task', function (Request $request) {
+    //
+});
+
+// 刪除任務
+Route::delete('/task/{task}', function (Task $task) {
+    //
+});
+
